d2382d72fa126f2904730af1681164c5fb6032c9
Phillip Rhodes <motley.crue.fan@gmail.com>
2015-12-22 08:54:20 -0500

    implements logical NOT operation

--- octave/nn_not.m
+++ octave/nn_not.m
@@ -1,1 +1,20 @@
 #!/usr/bin/octave -q
+
+# A basic Neural Network that implements the logical NOT operation
+
+printf( "Starting Neural Network simulation...\n")
+
+#         [ bias var, x1, x2]
+inputX1 = [1; 0 ];
+inputX2 = [1; 1 ];
+
+nnWeightsTheta = [ 10; -20];
+
+temp1 = (inputX1')*nnWeightsTheta;
+temp2 = (inputX2')*nnWeightsTheta;
+
+
+sigmoid(temp1)
+sigmoid(temp2)
+
+
